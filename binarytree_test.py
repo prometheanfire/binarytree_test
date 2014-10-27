aa4aaa4816bd00018d297bc64fdd3318ef364b7f
Matthew Thode <mthode@mthode.org>
2014-10-27 02:36:27 -0500

    added deployment (setup.py) and docs

--- binarytree_test.py
+++ binarytree_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Copyright 2013 Matthew Thode
+#Copyright 2014 Matthew Thode
 
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
@@ -21,6 +21,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
+    """An application that compares binary trees via HTTP POST
+
+    Takes post data in json format containing two binary trees, a subtree and a supertree.
+
+    Args:
+        The json data itself is the only argument this function takes (via POST).
+        Within the json data there are expected to be two binary trees.
+
+            supertree: The binary tree we search in.
+            subtree: The binary tree we search for.
+
+    Returns:
+        Returns a response code depending on match status and request formatting.
+
+        204: if the subtree is in the supertree
+        400: if the request if invalidly formatted
+        404: if the subtree is not in the supertree
+
+    Raises:
+        nothing
+    """
     try:
         data = request.data
         if data == b'':
@@ -29,17 +50,36 @@ def index():
     except ValueError:
         return '', 400
     if 'subtree' in binarytree_json and 'supertree' in binarytree_json:
-        if 'matching' == recursive_bintree_check(binarytree_json['supertree'],
-                                                 binarytree_json['subtree']):
+        code = recursive_bintree_check(binarytree_json['supertree'], binarytree_json['subtree'])
+        if code == 'matching':
             return '', 204
-        else:
+        elif code == 'no_match':
             return '', 404
+        else:
+            return '', 400
     else:
         return '', 400
 
 
 def recursive_bintree_check(supertree, subtree):
-    # check for formatting of datastructure
+    """Tries to find a subtree in a supertree,
+
+    Takes two binary trees and checks if one is a subset of another.
+
+    Args:
+        supertree: The binary tree we are searching in.
+        subtree: The binary tree we are searching for.
+
+    Returns:
+        The value of if it matches or not
+
+        matching: if the subtree is found
+        no_match: if the subtree is not found
+        invalid_format: if there is a formatting problem in the binary tree
+
+    Raises:
+        nothing
+    """
     if 'value' in subtree and supertree:
         if subtree['value'] == supertree['value']:
             if 'right' in subtree and 'right' in supertree:
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='binarytree_test',
+    version='1.0',
+    long_description=__doc__,
+    packages=[],
+    scripts=['binarytree_test.py'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask']
+)
