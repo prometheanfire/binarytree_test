6b6b17bd47ddd210832e74f22490ad45a39584d1
Matthew Thode <mthode@mthode.org>
2014-10-27 02:10:53 -0500

    working app, needs test suite, docs and setup.py

--- main.py
+++ main.py
@@ -16,72 +16,37 @@
 from flask import Flask, request
 import json
 
-binarytree_json = json.loads("""{
-"supertree": {
-"value": 1,
-"left": {
-"value": 5,
-"right": {
-"value": 3
-}
-},
-"right": {
-"value": 4,
-"left": {
-"value": 2
-},
-"right": {
-"value": 5,
-"left": {
-"value": 0
-},
-"right": {
-"value": 8
-}
-}
-}
-},
-"subtree": {
-"value": 4,
-"right": {
-"value": 5
-}
-}
-}""")
-
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def index():
     try:
-        binarytree_json = json.loads(request.data)
-    except:
-        # 400 error here (formatting)
-        binarytree_json = json.loads('{}')
-        pass
-    if 'subtree' and 'supertree' in binarytree_json:
+        data = request.data
+        if data == b'':
+            data = request.form.copy().popitem()[0]
+        binarytree_json = json.loads(data)
+    except ValueError:
+        return '', 400
+    if 'subtree' in binarytree_json and 'supertree' in binarytree_json:
         if 'matching' == recursive_bintree_check(binarytree_json['supertree'],
                                                  binarytree_json['subtree']):
-            # 204 no content here
-            pass
+            return '', 204
         else:
-            # 404  here
-            pass
+            return '', 404
     else:
-        # 400 here (formatting)
-        pass
+        return '', 400
 
 
 def recursive_bintree_check(supertree, subtree):
     # check for formatting of datastructure
     if 'value' in subtree and supertree:
         if subtree['value'] == supertree['value']:
-            if 'right' in subtree and supertree:
+            if 'right' in subtree and 'right' in supertree:
                 code = recursive_bintree_check(supertree['right'], subtree['right'])
                 if code == 'matching':
                     return code
-            if 'left' in subtree and supertree:
+            if 'left' in subtree and 'left' in supertree:
                 code = recursive_bintree_check(supertree['left'], subtree['left'])
                 if code == 'matching':
                     return code
@@ -93,15 +58,13 @@ def recursive_bintree_check(supertree, subtree):
                 if code == 'matching':
                     return code
             if 'left' in supertree:
-                recursive_bintree_check(supertree['left'], subtree)
-            if 'right' or 'left' not in supertree:
+                code = recursive_bintree_check(supertree['left'], subtree)
+                if code == 'matching':
+                    return code
+            if 'right' not in supertree or 'left' not in supertree:
                 return 'no_match'
     else:
         return 'invalid_format'
 
 if __name__ == '__main__':
-    if 'subtree' and 'supertree' in binarytree_json:
-        foo = recursive_bintree_check(binarytree_json['supertree'], binarytree_json['subtree'])
-        print(foo)
-    else:
-        print('invalid_format')
+    app.run()
